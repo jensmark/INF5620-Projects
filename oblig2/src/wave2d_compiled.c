c9387e8a15bde4dc0b3314da28768d1a130aa044
Jens Kristoffer Reitan Markussen <jenskristoffer@ubuntu.(none)>
2013-10-12 18:04:28 +0200

    update

---
+++ oblig2/src/wave2d_compiled.c
@@ -0,0 +1,14 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+void ic(){
+	printf("Unimplemented function ic( ... )\n");
+}
+
+void scheme(){
+	printf("Unimplemented function scheme( ... )\n");
+}
+
+void bc(){
+	printf("Unimplemented function bc( ... )\n");
+}
